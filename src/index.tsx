d414545993b308242d9d9f89363569f287fed05d
AnnaKharatova <black-isida@yandex.ru>
2024-03-27 20:04:02 +0300

    deploy gh-pages: settings

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/index.ts'
+import { store } from './store/index.ts'   
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
